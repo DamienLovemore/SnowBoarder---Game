3fdb181effe5b7dbce7d87975e1ac2cae9bf71f2
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-09-28 14:06:07 -0300

    Added triggers for level finish and game over(player hits his head)

---
+++ Assets/Resources/Scripts/FinishLine.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("I`ve finished!");
+        }
+    }
+}
