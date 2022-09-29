f810e76926e9aa95e634b6d757b210151e741636
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-09-29 14:40:37 -0300

    Gameplay impromevent, and added speed boost to the game

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -1,23 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
     [SerializeField] float torqueAmount = 1f;
     //The component that controls the physics of the player
     private Rigidbody2D rb2d;
+    private SurfaceEffector2D surfaceAcellerator;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        //Similar to get Component but gets the first component of this
+        //type in the whole game, not just within this game object.
+        surfaceAcellerator = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if((Input.GetKey(KeyCode.LeftArrow)) || (Input.GetKey(KeyCode.A)))
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    private void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceAcellerator.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceAcellerator.speed = baseSpeed;
+        }
+    }
+
+    private void RotatePlayer()
+    {
+        if ((Input.GetKey(KeyCode.LeftArrow)) || (Input.GetKey(KeyCode.A)))
         {
             rb2d.AddTorque(torqueAmount);
         }
